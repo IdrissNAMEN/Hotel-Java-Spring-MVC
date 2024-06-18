e4b987471a6a66d56065fe23ca5fa384f01ae457
Tchoumba <tchoumba@esiea.fr>
2024-06-18 21:19:38 +0200

    Update project

---
+++ ProjetDeGestionHoteliere/src/main/java/ApplicationWebGestioHoteliere/com/ProjetDeGestionHoteliere/GestionHotelereApplication.java
@@ -0,0 +1,13 @@
+package ApplicationWebGestioHoteliere.com.ProjetDeGestionHoteliere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHotelereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHotelereApplication.class, args);
+	}
+
+}
---
+++ ProjetDeGestionHoteliere/src/test/java/ApplicationWebGestioHoteliere/com/ProjetDeGestionHoteliere/GestionHotelereApplicationTests.java
@@ -0,0 +1,13 @@
+package ApplicationWebGestioHoteliere.com.ProjetDeGestionHoteliere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHotelereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
